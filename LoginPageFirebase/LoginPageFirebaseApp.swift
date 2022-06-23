d65bae154161dc8afcd7241bdc96e2ee1444f5c1
Franco Rod <francorodios@gmail.com>
2022-06-23 14:21:14 -0400

    Initial Commit

---
+++ LoginPageFirebase/LoginPageFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageFirebaseApp.swift
+//  LoginPageFirebase
+//
+//  Created by Franco Rodrigues on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
